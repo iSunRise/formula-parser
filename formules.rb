c0a788ee5dccf4157d59cf34e04dcbdb014f67a0
SergeiKaptelin <serhii.kaptelin@gmail.com>
2017-06-27 14:34:50 +0300

    update formules, create images array

--- formules.rb
+++ formules.rb
@@ -1,31 +1,95 @@
-load 'calculus/lib/calculus.rb'
+require 'tmpdir'
+require 'digest'
 
-def parse_tex_file filename
-  #create .odt from .docx
-  convert_docx_to_odt = IO.popen(["soffice", "--headless", "--convert-to", "odt", filename])
-  convert_docx_to_odt.close
-
-  # get name without .docx
-  name = filename.scan(/(.+?).docx/)
-  odt_file = name[0][0] + ".odt"
-  tex_file = name[0][0] + ".tex"
-
-  #create .tex from .odt
-  convert_odt_to_tex = IO.popen(["w2l", odt_file, tex_file])
-  convert_odt_to_tex.close
-
-  #create array with formules
-  tex_text = IO.read(tex_file)
-  tex_text.gsub!(/\s+/, "")
-  formules = []
-  formules << tex_text.scan(/\$(.*?)\$/)
-  formules << tex_text.scan(/{equation\*}(.*?){equation\*}/)
+# Renders expression to PNG image using <tt>latex</tt> and
+# <tt>dvipng</tt>
+# returns array to images
+def formula_to_png(formules, background = 'White', density = 700)
+  arr_images = []
+  temp_path = Dir.mktmpdir
+  Dir.chdir(temp_path) do
+    formules.each do |formula|
+      rand_word = ('a'..'z').to_a.shuffle.join
+      sha1 = Digest::SHA1.hexdigest rand_word
+      File.open("#{sha1}.tex", 'w') do |f|
+        f.write("\\documentclass{article}\n" +
+                 "\\usepackage{amsmath,amssymb}\n" +
+                 "\\begin{document}\n" +
+                 "\\thispagestyle{empty}\n" +
+                 "$$ #{formula} $$\n" +
+                 "\\end{document}\n")
+      end
+      `latex -interaction=nonstopmode #{sha1}.tex && dvipng -q -T tight -bg #{background} -D #{density.to_i} -o #{sha1}.png #{sha1}.dvi`
+      File.delete("#{sha1}.tex") if File.exists?("#{sha1}.tex")
+      File.delete("#{sha1}.aux") if File.exists?("#{sha1}.aux")
+      File.delete("#{sha1}.dvi") if File.exists?("#{sha1}.dvi")
+      File.delete("#{sha1}.log") if File.exists?("#{sha1}.log")
+      arr_images << File.join(temp_path, "#{sha1}.png") if $?.exitstatus.zero?
+    end
+  end
+  return arr_images
 end
 
-# formules = parse_tex_file '4.docx'
+def parse_tex_file docx_path
+  arr_path_filename = File.split(docx_path)
+  outdir = arr_path_filename[0]
+  docx_file = arr_path_filename[1]
 
-# formules.each do |str|
-#   puts str
-# end
+  #create .odt from .docx
+  `soffice --headless --convert-to odt --outdir #{outdir} #{docx_file}`
 
-Calculus::Expression.new("t/coprod_{i}^{t}{t}=p\\int _{q}^{7}{p}\\ast 67+r", :parse => false).to_png
+  # alternative
+  # convert_docx_to_odt = IO.popen(["soffice", "--headless", "--convert-to", "odt", "--outdir", outdir, docx_file])
+  # convert_docx_to_odt.close
+
+  # get name without .docx
+  name = docx_file.scan(/(.+?)\.docx/)
+
+  # create path to odt and tex files
+  odt_path = File.expand_path(name[0][0] + ".odt", outdir)
+  tex_path = File.expand_path(name[0][0] + ".tex", outdir)
+
+  # create .tex from .odt
+  `w2l #{odt_path} #{tex_path}`
+
+  # alternative
+  # convert_odt_to_tex = IO.popen(["w2l", odt_path, tex_path])
+  # convert_odt_to_tex.close
+
+  # create array with formules
+  tex_text = File.read(tex_path)
+  tex_text.gsub!(/\s+/, "")
+  formules = []
+  arr1 = tex_text.scan(/\$(.*?)\$/im)
+  unless arr1.empty?
+    arr1.each do |level_1|
+      level_1.each do |level_2|
+        formules << level_2
+      end
+    end
+  end
+  arr2 = tex_text.scan(/\\begin{equation\*}(.+?)\\end{equation\*}/im)
+  unless arr2.empty?
+    arr2.each do |level_1|
+      level_1.each do |level_2|
+        formules << level_2
+      end
+    end
+  end
+  # --CHECK--
+  # formules.each do |str|
+  #   puts str
+  # end
+  # --CHECK--
+
+  imgages = formula_to_png(formules)
+
+
+  # delete .odt and .tex files
+  File.delete(odt_path) if File.exists?(odt_path)
+  File.delete(tex_path) if File.exists?(tex_path)
+end
+
+formules = parse_tex_file '/home/sergei/Programs/Work/plagiarizm/formula-parser/4.docx'
+
+
